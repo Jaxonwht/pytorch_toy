5e34338ca1afe4172296bbdedc473cd259efa1bf
Haotian Wang <haotian.wang@duke.edu>
2018-12-23 12:59:02 +0800

    trader questions

--- experiment.py
+++ experiment.py
@@ -1,8 +1,19 @@
-import torch
+import random
 
-x = torch.rand(1, 2, 3)
+NUMBER_EXPERIMENTS = 10**5
+p = 0.7
 
-y = torch.rand(3, 4)
-
-print(x[:, 0, :].size())
-
+counts = 0
+for i in range(NUMBER_EXPERIMENTS):
+    head = 0
+    tail = 1
+    count = 0
+    while head < tail:
+        toss = random.random()
+        if toss < 0.7:
+            head += 1
+        else:
+            tail += 1
+        count += 1
+    counts += count
+print(counts/NUMBER_EXPERIMENTS)
