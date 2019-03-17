542576bbc9ac8a997a4d1528e521544594b8fbbc
Haotian Wang <haotian.wang@duke.edu>
2019-03-17 18:40:23 -0400

    Fix dimension error with word2vec

--- word2vec/data_loader.py
+++ word2vec/data_loader.py
@@ -1,4 +1,5 @@
 from torch.utils.data import Dataset
+import torch
 
 
 class EmailDataset(Dataset):
@@ -18,9 +19,9 @@ class EmailDataset(Dataset):
         for wordseq in wordlists:
             for i in range(len(wordseq)):
                 for j in range(max(0, i - context_size), i):
-                    self.index_pair.append((self.word_dict[wordseq[i]], self.word_dict[wordseq[j]]))
+                    self.index_pair.append(torch.tensor([self.word_dict[wordseq[i]], self.word_dict[wordseq[j]]]))
                 for j in range(i + 1, min(len(wordseq), i + context_size + 1)):
-                    self.index_pair.append((self.word_dict[wordseq[i]], self.word_dict[wordseq[j]]))
+                    self.index_pair.append(torch.tensor([self.word_dict[wordseq[i]], self.word_dict[wordseq[j]]]))
 
     def __len__(self):
         return len(self.index_pair)
--- word2vec/word_to_vec.py
+++ word2vec/word_to_vec.py
@@ -3,26 +3,26 @@ import torch.nn as nn
 import torch.optim as optim
 from word2vec.data_loader import EmailDataset
 from torch.utils.data import DataLoader
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
 
-#  import matplotlib.pyplot as plt
-#  from sklearn.manifold import TSNE
 
-file_path = "../data/emails.train"
-out_model_state_dict = "model/model_state_dict.pt"
-out_optim_state_dict = "model/optim_state_dict.pt"
+FILE_PATH = "../data/democratic5000.train"
+OUT_MODEL_STATE_DICT = "model/model_state_dict.pt"
+OUT_OPTIM_STATE_DICT = "model/optim_state_dict.pt"
 if torch.cuda.is_available():
     gpu = torch.device("cuda")
 else:
     gpu = torch.device("cpu")
-context_size = 2
-batch_size = 32
-hidden_size = 500
-learning_rate = 1e-3
-epochs = 100
+CONTEXT_SIZE = 2
+BATCH_SIZE = 32
+HIDDEN_SIZE = 500
+LEARNING_RATE = 1e-3
+EPOCHS = 100
 
-email_data = EmailDataset(file_path, context_size)
+email_data = EmailDataset(FILE_PATH, CONTEXT_SIZE)
 vocab_size = email_data.get_number_of_tokens()
-my_data_loader = DataLoader(email_data, shuffle=True, batch_size=batch_size)
+my_data_loader = DataLoader(email_data, shuffle=True, batch_size=BATCH_SIZE)
 
 
 class Word2Vec(nn.Module):
@@ -35,31 +35,33 @@ class Word2Vec(nn.Module):
         return self.output(self.embed(x))
 
 
-model = Word2Vec(vocab_size, hidden_size).to(gpu)
+model = Word2Vec(vocab_size, HIDDEN_SIZE).to(gpu)
 loss_fn = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
 
-for e in range(epochs):
+for e in range(EPOCHS):
     for x, y in enumerate(my_data_loader):
-        input_vector = y[0].to(gpu)
+        input_vector = y[:, 0].to(gpu)
+        print(input_vector.size())
         target_pred = model(input_vector)
-        target_vector = y[1].to(gpu)
+        print(target_pred.size())
+        target_vector = y[:, 1].to(gpu)
         loss = loss_fn(target_pred, target_vector)
-        if (x + 1) % 1000 == 0:
+        if x % 1000 == 0:
             print("Epoch: {0}, Batch: {1}, loss: {2}".format(e, x, loss.item()))
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
-torch.save(model.state_dict(), out_model_state_dict)
-torch.save(optimizer.state_dict(), out_optim_state_dict)
+torch.save(model.state_dict(), OUT_MODEL_STATE_DICT)
+torch.save(optimizer.state_dict(), OUT_OPTIM_STATE_DICT)
 # model.load_state_dict(torch.load(out_model_state_dict))
 # optimizer.load_state_dict(torch.load(out_optim_state_dict))
 # model.eval()
 
 # plot all the points
-# with torch.no_grad():
-#     data = model.weight.cpu().numpy()
-#     X_embedded = TSNE(n_components=2).fit_transform(data)
-#     plt.plot(X_embedded, ".")
-#     plt.show()
+with torch.no_grad():
+    data = model.weight.cpu().numpy()
+    X_embedded = TSNE(n_components=2).fit_transform(data)
+    plt.plot(X_embedded, ".")
+    plt.show()
