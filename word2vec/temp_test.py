51dfa195c982ac8a366e7226ad7df129cfc9ff39
Haotian Wang <haotian.wang@duke.edu>
2019-03-18 14:08:56 -0400

    Add a simple test python file to test my word2vec, loss indeed decreases considerably

---
+++ word2vec/temp_test.py
@@ -0,0 +1,26 @@
+import torch
+from torch.nn import CrossEntropyLoss
+from torch.optim import Adam
+from word2vec.word_to_vec import Word2Vec
+
+input = torch.randint(0, 5, (10, ))
+target = torch.randint(0, 5, (10, ))
+
+model = Word2Vec(5, 2)
+optimizer = Adam(model.parameters(), lr=4)
+loss_fn = CrossEntropyLoss()
+
+for epoch in range(10000):
+    pred = model(input)
+    loss = loss_fn(pred, target)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    print("Epoch: {}, loss: {}".format(epoch, loss.item()))
+
+print(input)
+print("====")
+print(target)
+print("====")
+model.eval()
+print(torch.nn.Softmax()(model(input)))
