42b4c2793b7768e5abeecdc07a8eabf2188d75b7
Haotian Wang <haotian.wang@duke.edu>
2019-03-18 01:41:00 -0400

    Add start of string and end of string as special words to word2vec

--- word2vec/data_loader_comp.py
+++ word2vec/data_loader_comp.py
@@ -1,14 +1,17 @@
 from torch.utils.data import Dataset
 import torch
 
+END_OF_STRING = "&EOS"
+START_OF_STRING = "&SOS"
+
 
 class EmailDataset(Dataset):
     def __init__(self, file_path, context_size):
         super().__init__()
         wordlists = [line.strip().split(" ")[1:] for line in open(file_path)]
-        self.index_dict = {}
-        self.word_dict = {}
-        index = 0
+        self.index_dict = {0: START_OF_STRING, 1: END_OF_STRING}
+        self.word_dict = {START_OF_STRING: 0, END_OF_STRING: 1}
+        index = 2
         for wordseq in wordlists:
             for word in wordseq:
                 if word not in self.word_dict:
@@ -17,6 +20,8 @@ class EmailDataset(Dataset):
                     index += 1
         self.index_pair = []
         for wordseq in wordlists:
+            wordseq.insert(0, START_OF_STRING)
+            wordseq.append(END_OF_STRING)
             for i in range(len(wordseq)):
                 for j in range(max(0, i - context_size), i):
                     self.index_pair.append(torch.LongTensor([self.word_dict[wordseq[i]], self.word_dict[wordseq[j]]]))
