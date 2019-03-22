28632af7355d0ba2df1fce9e6d377df47f250e64
Haotian Wang <haotian.wang@duke.edu>
2019-03-21 23:01:16 -0400

    Remove embedding layer from encoding unit, because the decoder will share the embedding

--- vae/main_module/encoder_unit.py
+++ vae/main_module/encoder_unit.py
@@ -6,25 +6,20 @@ from torch.nn.utils.rnn import pad_sequence
 
 
 class Encoder(nn.Module):
-    def __init__(self, vocab, embed, hidden, num_layers=1, bidirectional=True, embedding_weights=None, max_seq_len=50):
+    def __init__(self, embed, hidden, num_layers=1, bidirectional=True, max_seq_len=50):
         super().__init__()
         self.max_seq_len = max_seq_len
-        if embedding_weights:
-            self.word_embed = nn.Embedding.from_pretrained(embeddings=embedding_weights)
-        else:
-            self.word_embed = nn.Embedding(vocab, embed)
         self.mu = nn.GRU(input_size=embed, hidden_size=hidden, num_layers=num_layers,
                          bidirectional=bidirectional, batch_first=True)
         self.logvar = nn.GRU(input_size=embed, hidden_size=hidden, num_layers=num_layers, bidirectional=bidirectional,
                              batch_first=True)
 
-    def forward(self, input, lengths):
+    def forward(self, x, lengths):
         '''
-        :param input: list of tensors of variable lengths, len(list) = batch_size
+        :param x: list of tensors of variable lengths, len(list) = batch_size
         :param lengths: [batch_size]
         :return: (out, hidden, lengths, kl_loss), out is [batch, padded_seq_len, 2 x encoder_hidden_dim], hidden is [batch, 2 x encoder_hidden_dim], lengths is a list [batch_size], kl_loss is scalar
         '''
-        x = [self.word_embed(seq) for seq in input]
         # x = [batch_size, padded_seq_len (not necessarily the max seq len), embedding_dim]
         x = pad_sequence(x, batch_first=True)
         x = pack_padded_sequence(x, lengths=lengths, batch_first=True)
@@ -66,8 +61,10 @@ class Encoder(nn.Module):
 
 if __name__ == "__main__":
     input = [torch.tensor([1, 2, 3]), torch.tensor([4, 5])]
+    embedding = nn.Embedding(6, 2)
+    input = [embedding(x) for x in input]
     lengths = torch.tensor([3, 2])
-    model = Encoder(6, 2, 10)
+    model = Encoder(2, 10)
     out, hidden, lengths, kl_loss = model(input, lengths)
     print(out.size())
     print(hidden.size())
