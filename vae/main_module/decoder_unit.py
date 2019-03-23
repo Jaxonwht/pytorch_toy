5595924cc56797371bbd4aa1046d8bb0a0d2bf05
Haotian Wang <haotian.wang@duke.edu>
2019-03-23 15:04:17 -0400

    finish decoder method signature

--- vae/main_module/decoder_unit.py
+++ vae/main_module/decoder_unit.py
@@ -1,13 +1,22 @@
 import torch.nn as nn
+from vae.main_module.attention_unit import Attention
 
 class Decoder(nn.Module):
-    def __init__(self):
+    def __init__(self, embed, encoder_hidden, decoder_hidden, vocab_size):
         super().__init__()
+        self.gru_cell = nn.GRUCell(input_size=embed, hidden_size=decoder_hidden)
+        self.attention = Attention(encoder_hidden_dim=encoder_hidden, decoder_hidden_dim=decoder_hidden)
+        self.interpret = nn.Linear(in_features=decoder_hidden, out_features=vocab_size)
 
-    def forward(self, input, hidden, encoder_outs, lengths):
+    def forward(self, input, hidden, encoder_outs, lengths, teacher_forcing_ratio=0):
         '''
-        :param input: [batch,
-        :param hidden:
-        :param encoder_outs:
-        :param lengths:
+        :param input: list, len(input) = batch. Each entry is [variable_seq_len, embed_dim]
+        :param hidden: [decoder_hidden_dim, batch]
+        :param encoder_outs: [batch, max_seq_len, 2 x encoder_hidden_dim]
+        :param lengths: [batch]
+        :param teacher_forcing_ratio: scalar
         '''
+        for seq_len in range(encoder_outs.size()[1]):
+            context = self.attention(encoder_outs, hidden, lengths)
+            # context = [batch, 2 x encoder_hidden_dim]
+
