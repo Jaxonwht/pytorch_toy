52e9ce06018d7e09a767a197bcbbd73549b9b29a
Haotian Wang <haotian.wang@duke.edu>
2019-03-23 23:19:42 -0400

    Finished coding decoder. Passed dimensionality test

--- vae/main_module/decoder_unit.py
+++ vae/main_module/decoder_unit.py
@@ -1,51 +1,56 @@
-import torch.nn as nn
 import torch
-from vae.main_module.attention_unit import Attention
+import torch.nn as nn
 from torch.nn.utils.rnn import pad_sequence
 
+from vae.main_module.attention_unit import Attention
+
+
 class Decoder(nn.Module):
-    def __init__(self, embed, encoder_hidden, decoder_hidden, vocab_size):
+    def __init__(self, encoder_hidden, decoder_hidden, embedding_layer):
         super().__init__()
-        self.embed = embed
+        self.embedding = embedding_layer
+        vocab_size = embedding_layer.weight.size()[0]
+        embed = embedding_layer.weight.size()[1]
         self.gru_cell = nn.GRUCell(input_size=embed + 2 * encoder_hidden, hidden_size=decoder_hidden)
         self.attention = Attention(encoder_hidden_dim=encoder_hidden, decoder_hidden_dim=decoder_hidden)
         self.interpret = nn.Linear(in_features=decoder_hidden, out_features=vocab_size)
+        self.token_activation = nn.LeakyReLU()
 
     def forward(self, input, hidden, encoder_outs, lengths, teacher_forcing_ratio=0.5):
         '''
-        :param input: list, len(input) = batch. Each entry is [variable_seq_len, embed_dim]
-        :param hidden: [decoder_hidden_dim, batch]
+        :param input: list, len(input) = batch. Each entry is [variable_seq_len]
+        :param hidden: [batch, hidden]
         :param encoder_outs: [batch, max_seq_len, 2 x encoder_hidden_dim]
         :param lengths: [batch]
         :param teacher_forcing_ratio: scalar
         '''
+        reconstruction_loss = torch.zeros(1)
+        input = [self.embedding(token) for token in input]
         paddded_input = pad_sequence(input, batch_first=True)
         # padded_input = [batch, max_seq_len, embed]
-        outs = torch.zeros(len(lengths), encoder_outs.size()[1], self.embed)
-        # outs = [batch, max_seq_len, embed_dim]
-        outs[:, 0, :] = paddded_input[:, 0, :]
+        out = torch.zeros(len(lengths), encoder_outs.size()[1], self.embedding.weight.size()[0])
+        # out = [batch, max_seq_len, vocab_size]
+        out[:, 0, 0] = torch.ones(len(lengths))
         for index in range(1, encoder_outs.size()[1]):
             context = self.attention(encoder_outs, hidden, lengths)
             # context = [batch, 2 x encoder_hidden_dim]
             if torch.rand(1) < teacher_forcing_ratio:
-                selected = input[:, index - 1, :]
+                selected = paddded_input[:, index - 1, :]
             else:
-                selected = outs[:, index - 1, :]
+                selected = self.embedding(torch.argmax(out[:, index - 1, :], dim=1))
             # selected = [batch, embed]
             modified_input = torch.cat((selected, context), dim=1)
             # modified_input = [batch, embed + 2 x encoder_hidden_dim]
-            hidden = self.gru_cell(modified_input, hidden.t())
-            outs[:, index, :] = hidden
-        return outs
+            hidden = self.gru_cell(modified_input, hidden)
+            out[:, index, :] = self.token_activation(self.interpret(hidden))
+        return out
+
 
 if __name__ == "__main__":
-    model = Decoder(3, 2, 2, 10)
-    input = [torch.randn(5, 3), torch.randn(4, 3), torch.randn(2, 3)]
+    model = Decoder(3, 2, nn.Embedding(7, 10))
+    input = [torch.randint(7, (5,)), torch.randint(7, (4,)), torch.randint(7, (2,))]
     lengths = torch.tensor([5, 4, 2])
-    hidden = torch.randn(2, 3)
-    encoder_outs = torch.randn(3, 5, 2 * 2)
+    hidden = torch.randn(3, 2)
+    encoder_outs = torch.randn(3, 5, 2 * 3)
     outs = model(input, hidden, encoder_outs, lengths)
     print(outs.size())
-
-
-
