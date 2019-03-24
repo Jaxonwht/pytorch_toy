be23c70ce4d861072ec106a558357fd2d12279c0
Haotian Wang <haotian.wang@duke.edu>
2019-03-24 03:30:32 -0400

    Change rand_like to randn_like to perform correct reparameterization

--- vae/main_module/encoder_unit.py
+++ vae/main_module/encoder_unit.py
@@ -56,7 +56,7 @@ class Encoder(nn.Module):
         :return: [embedding_dim]
         '''
         std = torch.exp(0.5 * logvar)
-        eps = torch.rand_like(std)
+        eps = torch.randn_like(std)
         return mu + torch.mul(eps, std)
 
     def kl_convergence_loss(self, mu, logvar):
