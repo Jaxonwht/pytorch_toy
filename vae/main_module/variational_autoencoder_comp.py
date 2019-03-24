0ef2a9b2bd05bcaf42d6a2b6bec0d73d5b608c25
Haotian Wang <haotian.wang@duke.edu>
2019-03-24 19:55:06 -0400

    Finished compatibility version of vae

---
+++ vae/main_module/variational_autoencoder_comp.py
@@ -0,0 +1,71 @@
+import torch.nn as nn
+import torch.optim
+from torch.nn.modules.linear import Linear
+from vae.main_module.decoder_unit_comp import Decoder
+from torch.nn.modules.sparse import Embedding
+from torch.autograd import Variable
+
+from vae.data_loader.data_loader_comp import VAEData
+from vae.main_module.encoder_unit_comp import Encoder
+
+
+class VAE(nn.Module):
+    def __init__(self, embed, encoder_hidden, decoder_hidden, embedding_weights=None, vocabulary=None):
+        super().__init__()
+        if vocabulary:
+            embedding = Embedding(vocabulary, embed)
+        else:
+            embedding = Embedding(embedding_weights.size()[0], embed)
+            embedding.weight.data = embedding_weights
+        self.encoder = Encoder(embedding_layer=embedding, hidden=encoder_hidden)
+        self.translator = Linear(in_features=encoder_hidden * 2, out_features=decoder_hidden)
+        self.translator_activation = nn.LeakyReLU()
+        self.decoder = Decoder(encoder_hidden=encoder_hidden, decoder_hidden=decoder_hidden, embedding_layer=embedding)
+
+    def forward(self, x, lengths):
+        '''
+        :param x: list of tensors, len(list) = batch, each tensor is [variable_seq_len]
+        :param lengths: [batch]
+        '''
+        encoder_outs, encoder_hidden, kl_loss = self.encoder(x, lengths)
+        decoder_hidden = self.translator_activation(self.translator(encoder_hidden))
+        out = self.decoder(x, decoder_hidden, encoder_outs, lengths)
+        return out, kl_loss
+
+
+if __name__ == "__main__":
+    BATCH_SIZE = 32
+    MAX_SEQ_LEN = 50
+    ENCODER_HIDDEN_SIZE = 200
+    DECODER_HIDDEN_SIZE = 200
+    LEARNING_RATE = 1e-3
+    EPOCHS = 300
+    EMBEDDING_SIZE = 500
+    VOCAB = "../../data/classtrain.txt"
+    TRAINING = "../../data/democratic_only.dev.en"
+    WORD2VEC_WEIGHT = "../../word2vec/model/model_state_dict.pt"
+
+    training_dataset = VAEData(filepath=TRAINING, vocab_data_file=VOCAB, max_seq_len=MAX_SEQ_LEN)
+    model = VAE(embed=EMBEDDING_SIZE, encoder_hidden=ENCODER_HIDDEN_SIZE, decoder_hidden=DECODER_HIDDEN_SIZE, embedding_weights=torch.load(WORD2VEC_WEIGHT)["embed.weight"]).cuda()
+    optim = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
+    loss_fn = nn.CrossEntropyLoss(ignore_index=-1)
+    for epoch in range(EPOCHS):
+        for batch in range(len(training_dataset) // BATCH_SIZE):
+            input = [Variable(training_dataset[batch * BATCH_SIZE + i].cuda()) for i in range(BATCH_SIZE)]
+            input.sort(key=lambda seq: len(seq), reverse=True)
+            lengths = [len(seq) for seq in input]
+            out, kl_loss = model(input, lengths)
+            padded_input = Variable(torch.zeros(len(lengths), lengths[0]).type(torch.LongTensor).cuda())
+            # padded_input = [batch, max_seq_len]
+            padded_input.fill_(-1)
+            for batch_index in range(len(lengths)):
+                padded_input[batch_index, :lengths[batch_index]] = input[batch_index]
+            out = out.permute(0, 2, 1)
+            # out: [batch, max_seq_len, vocab_size] -> [batch, vocab_size, max_seq_len]
+            total_loss = kl_loss
+            for token_index in range(1, lengths[0]):
+                total_loss += loss_fn(out[:, :, token_index], padded_input[:, token_index])
+            optim.zero_grad()
+            total_loss.backward()
+            optim.step()
+            print("Epoch {}, Batch {}, loss {}".format(epoch, batch, total_loss[0]))
