19de6c191ba9132290f2dc56d2fce68347f32185
Haotian Wang <haotian.wang@duke.edu>
2019-03-29 23:58:47 -0400

    Plot loss vs time

---
+++ vae/model/plot_loss.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+with open("slurm-51164028.out") as f:
+    i = 0
+    for line in f:
+        plt.scatter(i, float(line.split(" ")[9][:-1]))
+        i += 1
+plt.show()
