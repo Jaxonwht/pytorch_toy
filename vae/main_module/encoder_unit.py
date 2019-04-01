fix encoder kl loss and its ratio to the reconstruction loss

vae/main_module/encoder_unit.py
@@ -4,6 +4,7 @@ from torch.nn.utils.rnn import pack_padded_sequence
 from torch.nn.utils.rnn import pack_sequence
 from torch.nn.utils.rnn import pad_packed_sequence
 from torch.nn.utils.rnn import pad_sequence
+import matplotlib.pyplot as plt
 
 
 class Encoder(nn.Module):
@@ -27,8 +28,8 @@ class Encoder(nn.Module):
             input = [self.embedding(token) for token in x]
             x = pad_sequence(input, batch_first=True)
             x = pack_padded_sequence(x, lengths=lengths, batch_first=True)
-            mu_out, mu_hidden = self.mu(x)
-            logvar_out, logvar_hidden = self.logvar(x)
+            mu_out, _ = self.mu(x)
+            logvar_out, _ = self.logvar(x)
             # out is a padded seq
             # hidden = [2, batch, encoder_hidden_dim]
             # hidden can be separated into part = hidden.view(num_layers, num_directions, batch, hidden_size)
@@ -43,11 +44,8 @@ class Encoder(nn.Module):
                 kl_loss = kl_loss.add(
                     self.kl_convergence_loss(mu_out_list[batch, :lengths[batch].item()],
                                              logvar_out_list[batch, :lengths[batch].item()]))
-            kl_loss = kl_loss.add(self.kl_convergence_loss(mu_hidden, logvar_hidden)).div(len(lengths))
-            hidden = self.sample(mu_hidden, logvar_hidden)
-            # out = [batch, max_seq_len, 2 x encoder_hidden_dim]
-            # hidden = [2, batch, encoder_hidden_dim]
-            hidden = torch.cat((hidden[0], hidden[1]), dim=1)
+            kl_loss = kl_loss.div(len(lengths))
+            hidden = out[:, -1, :]
             # hidden = [batch, 2 x encoder_hidden_dim]
             return out, hidden, kl_loss
         else:

vae/main_module/encoder_unit_comp.py
@@ -28,8 +28,8 @@ class Encoder(nn.Module):
             for seq in range(len(lengths)):
                 input[seq, :lengths[seq], :] = x[seq]
             x = pack_padded_sequence(input, lengths=lengths, batch_first=True)
-            mu_out, mu_hidden = self.mu(x)
-            logvar_out, logvar_hidden = self.logvar(x)
+            mu_out, _ = self.mu(x)
+            logvar_out, _ = self.logvar(x)
             # out is a padded seq
             # hidden = [2, batch, encoder_hidden_dim]
             # hidden can be separated into part = hidden.view(num_layers, num_directions, batch, hidden_size)
@@ -44,12 +44,10 @@ class Encoder(nn.Module):
                 kl_loss = kl_loss.add(
                     self.kl_convergence_loss(mu_out_list[batch, :lengths[batch]],
                                              logvar_out_list[batch, :lengths[batch]]))
-            kl_loss = kl_loss.add(self.kl_convergence_loss(mu_hidden, logvar_hidden)).div(len(lengths))
-            hidden = self.sample(mu_hidden, logvar_hidden)
+            kl_loss = kl_loss.div(len(lengths))
+            hidden = out[:, -1, :]
             # out = [batch, max_seq_len, 2 x encoder_hidden_dim]
             # hidden = [2, batch, encoder_hidden_dim]
-            hidden = torch.cat((hidden[0], hidden[1]), dim=1)
-            # hidden = [batch, 2 x encoder_hidden_dim]
             return out, hidden, kl_loss
         else:
             input = Variable(torch.zeros(len(lengths), lengths[0], self.embedding.weight.size()[1]).cuda())

vae/main_module/variational_autoencoder.py
@@ -49,10 +49,11 @@ if __name__ == "__main__":
     TRAINING = "../../data/mixed_train.txt"
     WORD2VEC_WEIGHT = "../../word2vec/model/model_state_dict.pt"
     TESTING = "../../data/democratic_only.test.en"
-    MODEL_FILE_PATH = "../model/checkpoint.pt"
+    PRETRAINED_MODEL_FILE_PATH = "../model/checkpoint.pt"
+    MODEL_FILE_PATH = "../model/checkpoint_variation.pt"
     training = True
-    pretrained = False
-    variation = False
+    pretrained = True
+    variation = True
 
     if training:
         training_dataset = VAEData(filepath=TRAINING, vocab_data_file=VOCAB, max_seq_len=MAX_SEQ_LEN)
@@ -60,7 +61,7 @@ if __name__ == "__main__":
                     device=my_device, vocabulary=training_dataset.get_vocab_size()).to(my_device)
         optim = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
         if pretrained:
-            model.load_state_dict(torch.load(MODEL_FILE_PATH)["model_state_dict"])
+            model.load_state_dict(torch.load(PRETRAINED_MODEL_FILE_PATH)["model_state_dict"])
             # optim.load_state_dict(torch.load(MODEL_FILE_PATH)["optimizer_state_dict"])
         loss_fn = nn.CrossEntropyLoss(ignore_index=-1, reduction="sum")
         for epoch in range(EPOCHS):
@@ -73,11 +74,11 @@ if __name__ == "__main__":
                 # padded_input = [batch, max_seq_len]
                 out = out.permute(0, 2, 1)
                 # out: [batch, max_seq_len, vocab_size] -> [batch, vocab_size, max_seq_len]
-                reconstruction_loss = loss_fn(out, padded_input)
+                reconstruction_loss = loss_fn(out, padded_input) / BATCH_SIZE
                 # reconstruction_loss = torch.zeros(1, device=my_device)
                 # for token_index in range(1, lengths[0]):
                 #     reconstruction_loss += loss_fn(out[:, :, token_index], padded_input[:, token_index])
-                total_loss = reconstruction_loss
+                total_loss = reconstruction_loss + kl_loss
                 optim.zero_grad()
                 total_loss.backward()
                 optim.step()
@@ -90,12 +91,12 @@ if __name__ == "__main__":
         testing_dataset = VAEData(filepath=TESTING, vocab_data_file=VOCAB, max_seq_len=MAX_SEQ_LEN, offset=0)
         model = VAE(embed=EMBEDDING_SIZE, encoder_hidden=ENCODER_HIDDEN_SIZE, decoder_hidden=DECODER_HIDDEN_SIZE,
                     device=my_device, vocabulary=testing_dataset.get_vocab_size()).to(my_device)
-        model.load_state_dict(torch.load("../model/checkpoint.pt")["model_state_dict"])
+        model.load_state_dict(torch.load(PRETRAINED_MODEL_FILE_PATH)["model_state_dict"])
         input = [testing_dataset[i].to(my_device) for i in range(BATCH_SIZE)]
         input.sort(key=lambda seq: len(seq), reverse=True)
         lengths = torch.tensor([len(seq) for seq in input]).to(my_device)
         with torch.no_grad():
-            out, _ = model(input, lengths, teacher_forcing_ratio=0)
+            out, _ = model(input, lengths, teacher_forcing_ratio=0, variation=variation)
         index_out = torch.argmax(out, dim=2)
         for i in range(len(index_out)):
             out = index_out[i].tolist()

vae/main_module/variational_autoencoder_comp.py
@@ -47,16 +47,17 @@ if __name__ == "__main__":
     VOCAB = "../../data/classtrain.txt"
     TRAINING = "../../data/mixed_train.txt"
     WORD2VEC_WEIGHT = "../../word2vec/model/model_state_dict.pt"
-    MODEL_FILE_PATH = "../model/checkpoint.pt"
+    PRETRAINED_MODEL_FILE_PATH = "../model/checkpoint.pt"
+    MODEL_FILE_PATH = "../model/checkpoint_variation.pt"
     pretrained = False
-    variation = False
+    variation = True
 
     training_dataset = VAEData(filepath=TRAINING, vocab_data_file=VOCAB, max_seq_len=MAX_SEQ_LEN)
     model = VAE(embed=EMBEDDING_SIZE, encoder_hidden=ENCODER_HIDDEN_SIZE, decoder_hidden=DECODER_HIDDEN_SIZE,
                 vocabulary=training_dataset.get_vocab_size()).cuda()
     optim = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
     if pretrained:
-        model.load_state_dict(torch.load(MODEL_FILE_PATH)["model_state_dict"])
+        model.load_state_dict(torch.load(PRETRAINED_MODEL_FILE_PATH)["model_state_dict"])
         # optim.load_state_dict(torch.load(MODEL_FILE_PATH)["optimizer_state_dict"])
     loss_fn = nn.CrossEntropyLoss(ignore_index=-1, size_average=False)
     for epoch in range(EPOCHS):
@@ -75,7 +76,7 @@ if __name__ == "__main__":
             reconstruction_loss = Variable(torch.zeros(1).cuda())
             for token_index in range(1, lengths[0]):
                 reconstruction_loss += loss_fn(out[:, :, token_index], padded_input[:, token_index])
-            total_loss = reconstruction_loss
+            total_loss = reconstruction_loss / BATCH_SIZE + kl_loss
             optim.zero_grad()
             total_loss.backward()
             optim.step()