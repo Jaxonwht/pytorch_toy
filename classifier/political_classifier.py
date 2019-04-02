5e96e22cdd801808c40dc434ca5ea7df9c31d39e
Haotian Wang <haotian.wang@duke.edu>
2019-04-02 14:37:50 -0400

    change hyperparameters of the classifier, added inference mode

--- classifier/political_classifier.py
+++ classifier/political_classifier.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 from torch.optim import Adam
+import torch.nn.functional as F
 
 from vae.data_loader.data_loader import VAEData
 
@@ -11,11 +12,11 @@ class Classifier(nn.Module):
         self.encoder = nn.GRU(input_size=vocab_size, hidden_size=rnn_hidden_dim, bidirectional=True,
                               num_layers=rnn_layers, batch_first=True)
         self.conv1 = nn.Conv1d(in_channels=rnn_layers * 2, out_channels=10, kernel_size=rnn_hidden_dim // 5)
-        self.conv2 = nn.Conv1d(in_channels=10, out_channels=50, kernel_size=rnn_hidden_dim // 10)
-        self.conv3 = nn.Conv1d(in_channels=50, out_channels=200, kernel_size=rnn_hidden_dim // 100)
+        self.conv2 = nn.Conv1d(in_channels=10, out_channels=20, kernel_size=rnn_hidden_dim // 10)
+        self.conv3 = nn.Conv1d(in_channels=20, out_channels=50, kernel_size=rnn_hidden_dim // 100)
         self.activation = nn.LeakyReLU()
         self.fc1 = nn.Linear(
-            in_features=(rnn_hidden_dim - rnn_hidden_dim // 5 - rnn_hidden_dim // 10 - rnn_hidden_dim // 100 + 3) * 200,
+            in_features=(rnn_hidden_dim - rnn_hidden_dim // 5 - rnn_hidden_dim // 10 - rnn_hidden_dim // 100 + 3) * 50,
             out_features=mid_hidden_dim)
         self.fc2 = nn.Linear(in_features=mid_hidden_dim, out_features=class_number)
 
@@ -35,7 +36,7 @@ if __name__ == "__main__":
         my_device = torch.device("cpu")
     BATCH_SIZE = 50
     MAX_SEQ_LEN = 50
-    RNN_HIDDEN_DIM = 300
+    RNN_HIDDEN_DIM = 150
     LEARNING_RATE = 1e-2
     EPOCHS = 300
     MID_HIDDEN = 50
@@ -45,34 +46,66 @@ if __name__ == "__main__":
     TESTING = "../data/democratic_only.test.en"
     PRETRAINED_MODEL_FILE_PATH = "model/checkpoint.pt"
     MODEL_FILE_PATH = "model/checkpoint.pt"
-    pretrained = False
+    pretrained = True
+    training = False
 
-    training_data = VAEData(filepath=TRAINING, vocab_file=VOCAB, max_seq_len=MAX_SEQ_LEN, data_file_offset=1,
-                            vocab_file_offset=1)
-    model = Classifier(vocab_size=training_data.get_vocab_size(), rnn_hidden_dim=RNN_HIDDEN_DIM, rnn_layers=2,
-                       mid_hidden_dim=MID_HIDDEN, class_number=2).to(my_device)
-    optim = Adam(model.parameters(), lr=LEARNING_RATE)
-    loss_fn = nn.CrossEntropyLoss()
-    for e in range(EPOCHS):
-        for batch in range(len(training_data) // BATCH_SIZE):
-            input = []
-            target = []
-            for i in range(BATCH_SIZE):
-                temp = training_data[batch * BATCH_SIZE + i].to(my_device)
-                # temp = [seq_len]
-                dummy = torch.zeros(len(temp), training_data.get_vocab_size(), device=my_device)
-                # dummy = [seq_len, vocab_size]
-                dummy.scatter_(1, temp.unsqueeze(1), 1)
-                input.append(dummy)
-                target.append((training_data.get_tag(batch * BATCH_SIZE + i), len(dummy)))
-            target.sort(key=lambda x: x[1], reverse=True)
-            target = torch.tensor([x[0] for x in target], device=my_device)
-            input.sort(key=lambda seq: len(seq), reverse=True)
-            pack = nn.utils.rnn.pack_sequence(input).to(my_device)
-            scores = model(pack)
-            loss = loss_fn(scores, target)
-            optim.zero_grad()
-            loss.backward()
-            optim.step()
-            if batch % 10 == 0:
-                print("Epoch {}, Batch {}, Loss {}".format(e, batch, loss.item()))
+    if training:
+        training_data = VAEData(filepath=TRAINING, vocab_file=VOCAB, max_seq_len=MAX_SEQ_LEN, data_file_offset=1,
+                                vocab_file_offset=1)
+        model = Classifier(vocab_size=training_data.get_vocab_size(), rnn_hidden_dim=RNN_HIDDEN_DIM, rnn_layers=2,
+                           mid_hidden_dim=MID_HIDDEN, class_number=2).to(my_device)
+        optim = Adam(model.parameters(), lr=LEARNING_RATE)
+        if pretrained:
+            model.load_state_dict(torch.load(PRETRAINED_MODEL_FILE_PATH)["model_state_dict"])
+            optim.load_state_dict(torch.load(PRETRAINED_MODEL_FILE_PATH)["optimizer_state_dict"])
+        loss_fn = nn.CrossEntropyLoss()
+        for e in range(EPOCHS):
+            for batch in range(len(training_data) // BATCH_SIZE):
+                input = []
+                target = []
+                for i in range(BATCH_SIZE):
+                    temp = training_data[batch * BATCH_SIZE + i].to(my_device)
+                    # temp = [seq_len]
+                    dummy = torch.zeros(len(temp), training_data.get_vocab_size(), device=my_device)
+                    # dummy = [seq_len, vocab_size]
+                    dummy.scatter_(1, temp.unsqueeze(1), 1)
+                    input.append(dummy)
+                    target.append((training_data.get_tag(batch * BATCH_SIZE + i), len(dummy)))
+                target.sort(key=lambda x: x[1], reverse=True)
+                target = torch.tensor([x[0] for x in target], device=my_device)
+                input.sort(key=lambda seq: len(seq), reverse=True)
+                pack = nn.utils.rnn.pack_sequence(input).to(my_device)
+                scores = model(pack)
+                loss = loss_fn(scores, target)
+                optim.zero_grad()
+                loss.backward()
+                optim.step()
+                if batch % 10 == 0:
+                    print("Epoch {}, Batch {}, Loss {}".format(e, batch, loss.item()))
+    else:
+        training_data = VAEData(filepath=TRAINING, vocab_file=VOCAB, max_seq_len=MAX_SEQ_LEN, data_file_offset=1,
+                                vocab_file_offset=1)
+        model = Classifier(vocab_size=training_data.get_vocab_size(), rnn_hidden_dim=RNN_HIDDEN_DIM, rnn_layers=2,
+                           mid_hidden_dim=MID_HIDDEN, class_number=2).to(my_device)
+        model.eval()
+        if pretrained:
+            model.load_state_dict(torch.load(PRETRAINED_MODEL_FILE_PATH)["model_state_dict"])
+        for e in range(EPOCHS):
+            for batch in range(len(training_data) // BATCH_SIZE):
+                input = []
+                target = []
+                for i in range(BATCH_SIZE):
+                    temp = training_data[batch * BATCH_SIZE + i].to(my_device)
+                    # temp = [seq_len]
+                    dummy = torch.zeros(len(temp), training_data.get_vocab_size(), device=my_device)
+                    # dummy = [seq_len, vocab_size]
+                    dummy.scatter_(1, temp.unsqueeze(1), 1)
+                    input.append(dummy)
+                    target.append((training_data.get_tag(batch * BATCH_SIZE + i), len(dummy)))
+                target.sort(key=lambda x: x[1], reverse=True)
+                target = torch.tensor([x[0] for x in target], device=my_device)
+                input.sort(key=lambda seq: len(seq), reverse=True)
+                pack = nn.utils.rnn.pack_sequence(input).to(my_device)
+                scores = model(pack)
+                print(F.softmax(scores, dim=1))
+
