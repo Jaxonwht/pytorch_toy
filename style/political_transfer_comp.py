1c5a9d743eb6ff5fff8caa1d043d50ecbb993ab8
Haotian Wang <haotian.wang@duke.edu>
2019-04-02 20:04:59 -0400

    more frequent saver

--- style/political_transfer_comp.py
+++ style/political_transfer_comp.py
@@ -85,10 +85,11 @@ if __name__ == "__main__":
                                                                                                                       0],
                                                                                                                   total_loss.data[
                                                                                                                       0]))
-        print("Saving checkpoints...")
-        torch.save(
-            {"Epoch": epoch, "KL Loss": kl_loss.data[0], "Reconstruction Loss": reconstruction_loss.data[0],
-             "Style Loss": style_loss.data[0],
-             "Total Loss": total_loss.data[0],
-             "model_state_dict": model.state_dict(),
-             "optimizer_state_dict": optim.state_dict()}, MODEL_FILE_PATH)
+            if batch % 1000 == 0:
+                print("Saving checkpoints...")
+                torch.save(
+                    {"Epoch": epoch, "KL Loss": kl_loss.data[0], "Reconstruction Loss": reconstruction_loss.data[0],
+                     "Style Loss": style_loss.data[0],
+                     "Total Loss": total_loss.data[0],
+                     "model_state_dict": model.state_dict(),
+                     "optimizer_state_dict": optim.state_dict()}, MODEL_FILE_PATH)
